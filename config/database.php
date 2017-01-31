53b2d1ba0052ac4a8b81cce4990e1eb28ca6822e
Tasha Casagni <casagni708@hotmail.com>
2017-01-30 22:03:33 -0600

    bug fix

--- config/database.php
+++ config/database.php
@@ -2,6 +2,13 @@
 
 return [
 
+// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+// $host = $url["host"];
+// $username = $url["user"];
+// $password = $url["pass"];
+// $database = substr($url["path"], 1);
+
     /*
     |--------------------------------------------------------------------------
     | PDO Fetch Style
